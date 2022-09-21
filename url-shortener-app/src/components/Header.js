bfff3926f491a24b9483ac6c5648ea64a7f04f4c
dojh111 <dojh111@gmail.com>
2022-09-21 11:07:05 +0800

    Add background and icon

--- url-shortener-app/src/components/Header.js
+++ url-shortener-app/src/components/Header.js
@@ -1,18 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { GiUnderwearShorts } from "react-icons/gi";
 
 const Header = ({ title }) => {
   return (
     <header>
-      <h1 className="header">{title}</h1>
+      <div className="title-container">
+        <h1 className="header">{title}</h1>
+        <h1 className="header">
+          <GiUnderwearShorts className="header" />
+        </h1>
+      </div>
+
       <p className="body">
-        Shorten your lengthy URLs with Shortify, the fastest URL shortener
-        available.
+        Shorten lengthy URLs with{" "}
+        <span style={{ fontWeight: "bold" }}>Shortify</span>, the fastest URL
+        shortener available!
       </p>
 
       <p className="body">
         Just paste a URL you want to shorten into the box below, and you will
-        get your shortened URL in no time.
+        get your shortened URL in no time!
       </p>
     </header>
   );
